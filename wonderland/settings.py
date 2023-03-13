96e281f58a01befd195f884c1c2df28381509012
Thephelpster <thephelpster1992@gmail.com>
2023-03-13 17:54:51 +0000

    deployment2

--- wonderland/settings.py
+++ wonderland/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['wonderland-tattoo.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://git.heroku.com/wonderland-tattoo.git', 'localhost']
 
 
 # Application definition
